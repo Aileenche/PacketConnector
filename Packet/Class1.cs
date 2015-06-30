f291860f89f1dadc9d735b7f85ef9acb277d11a9
Zeeshan Abid <zeeshan595@googlemail.com>
2015-07-01 00:30:09 +0100

    [STARTED]

---
+++ Packet/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace PacketConnector
+{
+    public enum PacketType
+    {
+        Registration,
+        Chat,
+        CloseConnection
+    }
+
+    [Serializable]
+    public class Packet
+    {
+        public List<string> data;
+        public int packetInt;
+        public bool packetBool;
+        public string senderID;
+        public PacketType packetType;
+
+        public Packet(PacketType type, string senderID)
+        {
+            data = new List<string>();
+            this.senderID = senderID;
+            this.packetType = type;
+        }
+        public Packet(byte[] packetBytes)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(packetBytes);
+
+            Packet p = (Packet)bf.Deserialize(ms);
+            ms.Close();
+            this.data = p.data;
+            this.packetInt = p.packetInt;
+            this.packetBool = p.packetBool;
+            this.senderID = p.senderID;
+            this.packetType = p.packetType;
+        }
+
+
+        public byte[] ToBytes()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+
+            bf.Serialize(ms, this);
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+
+            return bytes;
+        }
+    }
+}
